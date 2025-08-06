5718d6f3079ac8c95fb8573c92f4b21b016360ce
prachi2052 <127882331+prachi2052@users.noreply.github.com>
2025-08-06 19:58:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-const numbers = [2, 3, 1, 4, 5, 1, 2, 3, 1, 2]; // example 10 numbers
-
-const product = numbers.reduce((acc, num) => acc * num, 1);
-
-console.log("Product of 10 numbers:", product);
+public class MultiplyNumbers {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 4, 5, 1, 2, 3, 1, 2}; // example 10 numbers
+        int product = 1;
+        for (int n : nums) {
+            product *= n;
+        }
+        System.out.println("Product of 10 numbers: " + product);
+    }
+}
